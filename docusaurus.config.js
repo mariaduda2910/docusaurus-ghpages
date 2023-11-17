bb497e7fea5721c50cbeee6609b9acaa3831e859
maria <a79366@ualg.pt>
2023-11-17 11:44:37 +0000

    Alterei titulo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Maria',
+  title: 'maria',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
